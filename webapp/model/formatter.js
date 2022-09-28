a6f0fd4166cb2075c8f35d51be54407ee5928b22
iheb dhouibi <ihebdhouibi@polytechnicien.tn>
2022-09-28 14:34:35 +0100

    ExtendingWorkListTable/ language column done / colors not showing

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,19 +1,32 @@
-sap.ui.define([], function () {
+sap.ui.define([
+	"sap/ui/core/library"
+], function (coreLibrary) {
 	"use strict";
 
+	// shortcut for sap.ui.core.ValueState
+	var ValueState = coreLibrary.ValueState;
 	return {
 
 		/**
 		 * Rounds the number unit value to 2 digits
 		 * @public
+		 * @param {string} iValue the language code 
 		 * @param {string} sValue the number string to be rounded
 		 * @returns {string} sValue with 2 digits rounded
 		 */
-		numberUnit : function (sValue) {
-			if (!sValue) {
-				return "";
+		languageState : function(iValue) {
+			console.log("slm")
+			console.log(iValue)
+			if (iValue == 'E') {
+				console.log("english")
+				return ValueState.Error;
+			} else if (iValue == 'F') {
+				console.log("French")
+				return ValueState.Success;
+			} else {
+				console.log("deutsh")
+				return ValueState.Warning;
 			}
-			return parseFloat(sValue).toFixed(2);
 		}
 
 	};
